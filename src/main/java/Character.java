0245cbd795016cd9964e29e615f5936df472ae49
jovanplavsic <jovanplavsic05@gmail.com>
2025-05-07 15:13:17 -0500

    Add getters and setters

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,2 +1,35 @@
 public class Character {
+    protected String name;
+    protected int health;
+    protected int attack;
+    protected int defense;
+    protected int [] inventory;
+
+     public Character(String name, int health, int attack, int defense) {
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.defense = defense;
+    }
+
+    public String getName(){
+         return this.name;
+    }
+
+    public int getHealth(){
+         return this.health;
+    }
+
+    public void getStats(){
+         System.out.println("Attack: " + this.attack + "Defence: " + this.defense);
+    }
+
+    public boolean takeDamage(int damage){
+         this.health = this.health - damage;
+         if (this.health > 0){
+             return false;
+         }
+         return true;
+    }
+
 }
