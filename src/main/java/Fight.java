a5dc034f7b3479ed63061f4eafadb5a43c75cbe5
jovanplavsic <jovanplavsic05@gmail.com>
2025-05-09 09:31:00 -0500

    Add proper functionality in Fight class

--- src/main/java/Fight.java
+++ src/main/java/Fight.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class Fight implements Action{
+public class Fight implements Action {
     private Character player;
     private Monster guard;
     private final Random rng = new Random();
@@ -12,27 +12,68 @@ public class Fight implements Action{
 
 
     @Override
-    public String printAction(){
+    public String printAction() {
         return "Fight " + this.guard.getName();
     }
 
     @Override
-    public void execute(){
+    public void execute() {
 
-        boolean monsterUsesStrength = monster.getCraft() == 0;
-        int monsterSkill = monsterUsesStrength
-                           ? monster.getStrength()
-                           : monster.getCraft();
-        int playerSkill  = monsterUsesStrength
-                           ? player.getStrength()
-                           : player.getCraft();
+        boolean useStrength;
+        if (guard.getCraft() == 0) {
+            useStrength = true;
+        } else {
+            useStrength = false;
+        }
+
+        int monsterSkill;
+        if (useStrength) {
+            monsterSkill = guard.getStrength();
+        } else {
+            monsterSkill = guard.getCraft();
+        }
+
+        int playerSkill;
+        if (useStrength) {
+            playerSkill = player.getStrength();
+        } else {
+            playerSkill = player.getCraft();
+        }
+
+        Item right = player.rightHand();
+        int itemBonus = 0;
+        if (right != null) {
+            if (useStrength) {
+                itemBonus = right.getPower();
+            } else {
+                itemBonus = right.getPower();
+            }
+        }
+
+        int monsterRoll = rng.nextInt(6) + 1 + monsterSkill;
+        int playerRoll = rng.nextInt(6) + 1 + playerSkill + itemBonus;
+
+        System.out.println("You roll" + playerRoll + "vs. monster’s " + monsterRoll);
+
+        int diff = playerRoll - monsterRoll;
+        if (diff > 0) {
+            guard.takeDamage(diff);
+            System.out.println("You deal " + diff + " damage! Monster health is now " + guard.getHealth());
+            if (guard.getHealth() <= 0) {
+                System.out.println("Monster defeated!");
+            }
+        } else {
+            int damage = -diff;
+            int newPlayerHealth = player.getHealth() - damage;
+            player.takeDamage(diff);
+            System.out.println("Monster deals " + diff + " damage! Your health is now " + player.getHealth());
+        }
+
 
     }
 
     @Override
-    public String toString(){
-        Monster guard = door.getGuard();
-
-        return "Fight " + guard.getName();
+    public String toString() {
+        return "Fight " + this.guard.getName();
     }
 }
