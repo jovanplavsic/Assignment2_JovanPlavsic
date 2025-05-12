57e5472d669180870a90c536a93d68ad8a83635c
jovanplavsic <jovanplavsic05@gmail.com>
2025-05-12 18:51:34 -0500

    Update Monster class to extend Creature class

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,44 +1,6 @@
-public class Monster {
-    private String name;
-    protected int strength;
-    protected int craft;
-    protected int health;
-
+/** A Monster is just a Creature with no inventory. */
+public class Monster extends Creature {
     public Monster(String name, int strength, int craft, int health) {
-        this.name = name;
-        this.strength = strength;
-        this.craft = craft;
-        this.health = health;
+        super(name, strength, craft, health);
     }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public int getHealth() {
-        return this.health;
-    }
-
-    public String getDescription() {
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(this.name).append("\n")
-                .append("Str: ").append(this.strength).append(" ")
-                .append("Cft: ").append(this.craft).append(" ")
-                .append("Hth: ").append(this.health).append(" ");
-        return sb.toString();
-    }
-
-    public void takeDamage(int damage) {
-        this.health = this.health - damage;
-    }
-
-    public int getCraft() {
-        return this.craft;
-    }
-
-    public int getStrength() {
-        return this.strength;
-    }
-
 }
